a2afc2037bdd34499648115dd1a0da953e20c035
Gabriel Magpantay <rielmagpantay@gmail.com>
2025-12-26 17:23:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8432386314-93bd2",
+  "appId": "1:859225822614:web:97785198b0918d8492f9b7",
+  "apiKey": "AIzaSyAmmjJdbaiHlYYhMc3pOhk0jpU1kFuJ8XM",
+  "authDomain": "studio-8432386314-93bd2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "859225822614"
+};
