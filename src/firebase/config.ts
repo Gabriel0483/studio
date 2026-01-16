07c2f95cad75e35ba91bafe6771a89c0215096d5
Gabriel Magpantay <rielmagpantay@gmail.com>
2026-01-16 06:02:49 +0000

    Try fixing this error: `Console TypeError: Failed to fetch. Error source

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
   "projectId": "studio-8432386314-93bd2",
   "appId": "1:859225822614:web:97785198b0918d8492f9b7",
+  "storageBucket": "studio-8432386314-93bd2.appspot.com",
   "apiKey": "AIzaSyAmmjJdbaiHlYYhMc3pOhk0jpU1kFuJ8XM",
   "authDomain": "studio-8432386314-93bd2.firebaseapp.com",
+  "databaseURL": "https://studio-8432386314-93bd2-default-rtdb.firebaseio.com",
   "measurementId": "",
   "messagingSenderId": "859225822614"
 };
